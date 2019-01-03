495401ba4fa8a572fc1f293d76f7f7cd70190e64
MuraliChagarlamudi <murali.chagarlamudi@in.ibm.com>
2019-01-03 15:01:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); //express
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs'); //handlebars support
+
+app.use((req, res, next) => {  //express middleware
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} : ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to file server.log');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintanance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        message: 'Welcome to node-web-server'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        erroeMessage: 'Unable to handle request'
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server is up and running on port 30000');
+});
